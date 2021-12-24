039e2b184965e9fd8c5b5995418e339766add83b
volta2016 <giovoltadesign@gmail.com>
2021-12-24 18:32:07 -0300

    [next-js] social nav - update

--- data/index.js
+++ data/index.js
@@ -1,14 +1,20 @@
 export const dataSocial = [
   {
     id: 1,
-    url: "https://twitter.com/giovanniivolta",
-    alt: "Twitter profile",
-    image: "/twitter.svg",
+    url: "https://github.com/volta2016",
+    alt: "Github profile",
+    image: "/static/icon/github.svg",
   },
   {
     id: 2,
+    url: "https://twitter.com/giovanniivolta",
+    alt: "Twitter profile",
+    image: "/static/icon/twitter.svg",
+  },
+  {
+    id: 3,
     url: "https://www.linkedin.com/in/giovanni-frontend-developer/",
     alt: "Linkedin profile",
-    image: "/linkedin.svg",
+    image: "/static/icon/linkedin.svg",
   },
 ];

11c0ab9cbb49574c5fb11ab832cc218b02b01e39
volta2016 <giovoltadesign@gmail.com>
2022-01-07 12:13:37 -0300

    [next-js] workshop frontend post update

--- components/common/header/index.jsx
+++ components/common/header/index.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 import Image from "next/dist/client/image";
 import SocialMedia from "./SocialMedia";
+import Link from "next/link";
 
 const Header = () => (
   <header>
     <div className="container flex-d ai-c jc-sb py-s">
-      <Image
-        src="/static/images/logo-voltadev.svg"
-        alt="Vercel Logo"
-        width={120}
-        height={60}
-      />
+      <Link href="/" passHref>
+        <a>
+          <Image
+            src="/static/images/logo-voltadev.svg"
+            alt="Vercel Logo"
+            width={120}
+            height={60}
+          />
+        </a>
+      </Link>
       <SocialMedia />
     </div>
   </header>

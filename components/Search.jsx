f8a0752e0ba4db426275849e0c02e77fc8089183
volta2016 <giovoltadesign@gmail.com>
2022-02-06 13:00:24 -0300

    [next-js] icon tsx

--- components/Search.jsx
+++ components/Search.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import Image from "next/image";
+import { SearchIcon } from "../components/icon/search-icon";
 
 const Search = (props) => {
   // console.info({ props });
@@ -15,21 +17,9 @@ const Search = (props) => {
           placeholder="Buscar Artículos"
           className="px-4 py-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 block w-full rounded-md bg-white text-gray-900"
         />
-        <svg
-          height="35"
-          width="22"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="#654bc2"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-          />
-        </svg>
+        <figure>
+          <SearchIcon />
+        </figure>
       </form>
     </div>
   );
